c9ea9c6bc73654245a8561f8146b4a6698edb2d3
James Ren <chrenbingyan@gmail.com>
2018-02-04 16:28:56 -0800

    changed access permission for sql server

--- db/migrate/20180204223226_create_lists.rb
+++ db/migrate/20180204223226_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[5.1]
   def up
     create_table :lists do |t|
     	t.string "item_name", :null => false
-    	t.date "expirate_date", :default => 010118, :null =>false
+    	t.date "expirate_date", :null =>false
     end
   end
 
